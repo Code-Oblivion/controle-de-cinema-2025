796f6fabda562dea7dc91ce50c5084dc0a0191f5
AlexAraldi <alexanderaraldi@gmail.com>
2025-08-25 18:49:06 -0300

    feat: implementado Tests.Integracao ModuloGenero

---
+++ ControleDeCinema.Testes.Integracao/ModuloGenero/RepositorioGeneroEmOrm.Tests.cs
@@ -0,0 +1,86 @@
+﻿using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using ControleDeCinema.Testes.Integracao.Compartilhado;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloGenero;
+
+[TestClass]
+[TestCategory("Teste de Integração de Gênero")]
+public sealed class RepositorioGeneroEmOrm : TestFixture
+{
+    public void Deve_Cadastrar_GeneroCorretmente() 
+    {
+        // Arrange
+        var genero = new GeneroFilme("Terror");
+        // Act
+        _repositorioGenero?.Cadastrar(genero);
+        _dbContext.SaveChanges();
+
+        // Assert
+        var registroSelecionado = _repositorioGenero?.SelecionarRegistroPorId(genero.Id);
+
+        Assert.AreEqual(genero, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_GeneroCorretamente()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Terror");
+        _repositorioGenero?.Cadastrar(genero);
+        _dbContext.SaveChanges();
+        var gerenoEditado = new GeneroFilme("Comédia");
+
+        // Act
+        var conseguiuEditar = _repositorioGenero?.Editar(genero.Id, gerenoEditado);
+        _dbContext.SaveChanges();
+
+        // Assert
+        var registroSelecionado = _repositorioGenero?.SelecionarRegistroPorId(genero.Id);
+        Assert.IsTrue(conseguiuEditar);
+        Assert.AreEqual(genero, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_GeneroCorretamente()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Terror");
+        _repositorioGenero?.Cadastrar(genero);
+        _dbContext.SaveChanges();
+
+        // Act
+        var conseguiuExcluir = _repositorioGenero?.Excluir(genero.Id);
+        _dbContext.SaveChanges();
+
+        // Assert
+        var registroSelecionado = _repositorioGenero?.SelecionarRegistroPorId(genero.Id);
+        
+        Assert.IsTrue(conseguiuExcluir);
+        Assert.IsNull(registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_TodosGenerosCorretamente()
+    {
+        // Arrange
+        var genero1 = new GeneroFilme("Terror");
+        var genero2 = new GeneroFilme("Comédia");
+        var genero3 = new GeneroFilme("Ação");
+
+        List<GeneroFilme> generosEsperados = [genero1, genero2, genero3];
+        _repositorioGenero?.CadastrarEntidades(generosEsperados);
+        _dbContext.SaveChanges();
+
+        var generosEsperadosOrdenadas = generosEsperados
+            .OrderBy(g => g.Descricao)
+            .ToList();
+
+        // Act
+        var registrosSelecionados = _repositorioGenero?.SelecionarRegistros();
+        // Assert
+        CollectionAssert.AreEqual(generosEsperadosOrdenadas, registrosSelecionados);
+    }
+
+
+
+}
