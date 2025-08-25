2f30b1fd699a4c05368669fef4d1b26355c7a8ba
Agatha Sates <satesagatha@gmail.com>
2025-08-25 18:12:23 -0300

    feat: Criado base do modulo sessão

---
+++ ControleDeCinema.Testes.Integracao/ModuloSessao/RepositorioSessaoEmOrmTests.cs
@@ -0,0 +1,40 @@
+﻿using ControleDeCinema.Testes.Integracao.Compartilhado;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloSessao;
+
+[TestClass]
+[TestCategory("Testes de Integração de Sessão")]
+public sealed class RepositorioSessaoEmOrmTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Cadastrar_Disciplina_Corretamente() 
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Disciplina_Corretamente() 
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Disciplina_Corretamente() 
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_Disciplinas_Corretamente() 
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
