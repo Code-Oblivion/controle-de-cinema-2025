d8162559401975dee3b8221cc70c897ab7ce5173
Agatha Sates <satesagatha@gmail.com>
2025-08-26 14:23:03 -0300

    feat: Adicionado testes de integração de ingresso

---
+++ ControleDeCinema.Testes.Integracao/ModuloIngresso/RepositorioIngressoEmOrmTests.cs
@@ -0,0 +1,20 @@
+﻿using ControleDeCinema.Testes.Integracao.Compartilhado;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloIngresso;
+
+[TestClass]
+[TestCategory("Testes de Integração de Ingresso")]
+public sealed class RepositorioIngressoEmOrmTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Selecionar_Todos_Os_Ingressos_Do_Usuario_Corretamente()
+    {
+
+    }
+
+    [TestMethod]
+    public void Deve_Retornar_Null_Ao_Selecionar_Ingresso_Por_Id_Inexistente()
+    {
+
+    }
+}
